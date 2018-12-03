2b70bbb2ef482b17ef38c4480163339fd37adafe
weichenyu <weichenyu@ihsmf.com>
2018-12-03 09:41:24 +0800

    json

--- routes/download.js
+++ routes/download.js
@@ -1,21 +1,27 @@
 const router = require('koa-router')();
 var officegen = require('officegen');  //生成word
 var extract = require('pdf-text-extract')   // pdf 读取
+var pdftext = require('pdf-textstring'); 
+
 var fs = require('fs');
 var path = require('path');
-var pdfText = require('pdf-text')
-
 var filePath = path.join(__dirname, '../public/uploads/20181130/1543548463254.pdf');
-var buffer = fs.readFileSync(filePath);
-// fs.readFile(filePath, 'utf8', function(err, data){
-//     console.log(1);  
-// });
-
+let file = '';
+fs.readFile(filePath, 'utf8', function(err, data) {
+  if(err) throw err;
+  // data默认是一个Buffer对象，里面保存的就是一个一个的字节，(理解为字节数组)
+  // 把Buffer对象转换为字符串，调用toString()方法
+ file = data;
+})
+  // extract(filePath, function (err, pages) {
+  //   if (err) {
+  //     console.dir(err,3)
+  //     return
+  //   }
+  //   console.dir(pages,4)
+  // })
 var docx = officegen ({
-    'type': 'docx',
-	'subject': 'abc',
-	'keywords': 'dcf',
-	'description': 'sdf'
+    'type': 'docx'
 });
 router.prefix('/download');
 // 监听文档完成
@@ -31,7 +37,7 @@ docx.on ( 'error', function ( err ) {
 router.get('/file',(ctx)=> {
     ctx.set('Content-Type','application/vnd.openxmlformats-officedocument.presentationml.presentation',)
     ctx.set('Content-disposition','attachment; filename=surprise.docx')
-    ctx.body = '123213';
+    ctx.body = file.toString();
 });
 
 
